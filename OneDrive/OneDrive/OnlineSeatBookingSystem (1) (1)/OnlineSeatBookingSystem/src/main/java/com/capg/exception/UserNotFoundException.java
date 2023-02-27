14e6d47e1c007b2c7bd4517f9f3137ff427dfa81
PolampelliNiharika <124977755+PolampelliNiharika@users.noreply.github.com>
2023-02-27 14:50:29 +0530

    Add files via upload

---
+++ OneDrive/OneDrive/OnlineSeatBookingSystem (1) (1)/OnlineSeatBookingSystem/src/main/java/com/capg/exception/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.capg.exception;
+
+public class UserNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	String msg;
+
+	public UserNotFoundException(String msg) {
+		super(msg);
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+
+}
